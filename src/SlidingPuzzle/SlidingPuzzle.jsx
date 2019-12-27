fdf0261ed57f468976ce9d7e53a9250ef6dbcd0f
Joseph Hentges <joeypeterhentges@gmail.com>
2019-12-26 23:57:51 -0600

    initial comonent created

---
+++ src/SlidingPuzzle/SlidingPuzzle.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './SlidingPuzzle.css';
+
+export default class SlidingPizzle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="node-number">hi</p>
+      </div>
+    );
+  }
+}
