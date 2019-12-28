02897cdce58efdb8dc5bf71e089dc94f00f397be
Joseph Hentges <joeypeterhentges@gmail.com>
2019-12-28 15:57:21 -0600

    cleanup

--- src/SlidingPuzzle/Visualize/Visualize.js
+++ src/SlidingPuzzle/Visualize/Visualize.js
@@ -3,7 +3,6 @@ import { findOpenBox } from '../Algorithm/Helpers';
 export function Visualize(SliderPuzzle, moves) {
   const open = findOpenBox(SliderPuzzle.state.board.getMatrix());
   let pastMove = { row: open.row, column: open.column };
-  //const moves = getMoves();
   for (let i = 0; i < moves.length; i += 1) {
     const { row, column } = moves[i];
 
@@ -31,8 +30,6 @@ export function Visualize(SliderPuzzle, moves) {
 }
 
 const getDirection = (currentRow, currentColumn, newRow, newColumn) => {
-  //console.log(currentRow, currentColumn);
-  //console.log(newRow, newColumn);
   if (currentRow < newRow) {
     return 'move-down';
   } else if (currentRow > newRow) {
@@ -44,25 +41,4 @@ const getDirection = (currentRow, currentColumn, newRow, newColumn) => {
   } else {
     return '';
   }
-};
-
-const getMoves = () => {
-  return [
-    {
-      row: 1,
-      column: 2
-    },
-    {
-      row: 1,
-      column: 1
-    },
-    {
-      row: 2,
-      column: 1
-    },
-    {
-      row: 2,
-      column: 2
-    }
-  ];
 };
