594dba8d6dab60ecfaeccf1a5a9ce3fb3357afef
Joseph Hentges <joeypeterhentges@gmail.com>
2019-12-30 22:58:37 -0600

    add some filler boards with solutions

--- src/SlidingPuzzle/Board/Board.js
+++ src/SlidingPuzzle/Board/Board.js
@@ -23,8 +23,7 @@ export default class Board {
   }
 
   getNewBoard(size) {
-    //return [1, 2, 3, 4, 0, 5, 7, 8, 6];
-    //return [1, 6, 2, 4, 9, 5, 3, 7, 0, 10, 11, 8, 13, 14, 15, 12];
+    return [1, 6, 8, 7, 4, 5, 3, 0, 2];
     return scramble(Array.from({ length: size * size }, (_, b) => b));
   }
 
